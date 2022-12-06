c7e5618a1d16db8cec60c728b5450a944eefaeb4
DouglasCBS <douglascesar12@gmail.com>
2022-12-06 14:22:48 -0300

    Finish the function that plays the game once
    Add answers if the computer plays paper or scissors

--- script.js
+++ script.js
@@ -22,13 +22,40 @@ function playRound(playerChoice, computerChoice) {
     if (computerChoice === "ROCK") {
         switch (playerChoice) {
             case "ROCK":
-                return "It's a draw! Try again."
+                return "It's a draw!"
                 break;
             case "PAPER":
-                return "You win! Paper beats Rock"
+                return "You win! Paper beats Rock."
                 break;
             case "SCISSORS":
-                return "You lose! Rock beats Scissors"
+                return "You lose! Rock beats Scissors."
+        }
+    }
+    else if (computerChoice === "PAPER") {
+        switch (playerChoice) {
+            case "ROCK":
+                return "You lose! Paper beats Rock."
+                break;
+            case "PAPER":
+                return "It's a draw!"
+                break;
+            case "SCISSORS":
+                return "You win! Scissors beats Paper."
+                break;
+        }
+    }
+    else if (computerChoice === "SCISSORS") {
+        switch (playerChoice) {
+            case "ROCK":
+                return "You win! Rock beats Scissors."
+                break;
+            case "PAPER":
+                return "You lose! Scissors beats Paper."
+                break;
+            case "SCISSORS":
+                return "It's a draw!"
+                break;
+
         }
     }
 }
