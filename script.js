5847d404f0794a1d30524a24ca4bff6b479c59ad
DouglasCBS <douglascesar12@gmail.com>
2022-12-06 15:51:23 -0300

    Add game function
    Add a function that keeps tab of score and plays 5 rounds
    after 5 rounds the function alerts who won

--- script.js
+++ script.js
@@ -12,34 +12,35 @@ function getComputerChoice() {
         return "SCISSORS"
     }
 }
-
-let playerChoice = "ROCK"
-let computerChoice = getComputerChoice()
-
-console.log(computerChoice)
-
+let countWinner = 0
+let countLoser = 0
 function playRound(playerChoice, computerChoice) {
     if (computerChoice === "ROCK") {
         switch (playerChoice) {
             case "ROCK":
-                return "It's a draw!"
+                return "Tie!"
                 break;
             case "PAPER":
+                ++countWinner
                 return "You win! Paper beats Rock."
                 break;
             case "SCISSORS":
+                ++countLoser
                 return "You lose! Rock beats Scissors."
+                break;
         }
     }
     else if (computerChoice === "PAPER") {
         switch (playerChoice) {
             case "ROCK":
+                ++countLoser
                 return "You lose! Paper beats Rock."
                 break;
             case "PAPER":
-                return "It's a draw!"
+                return "Tie!"
                 break;
             case "SCISSORS":
+                ++countWinner
                 return "You win! Scissors beats Paper."
                 break;
         }
@@ -47,17 +48,38 @@ function playRound(playerChoice, computerChoice) {
     else if (computerChoice === "SCISSORS") {
         switch (playerChoice) {
             case "ROCK":
-                return "You win! Rock beats Scissors."
+                ++countWinner
+                return "You win! Rock beats Scissors"
                 break;
             case "PAPER":
+                ++countLoser
                 return "You lose! Scissors beats Paper."
                 break;
             case "SCISSORS":
-                return "It's a draw!"
+                return "Tie!"
                 break;
 
         }
     }
 }
 
-console.log(playRound(playerChoice, computerChoice))
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let computerChoice = getComputerChoice()
+        let playerChoice = prompt("Rock, paper or scissors?").toUpperCase()
+        alert(playRound(playerChoice, computerChoice))
+        
+        
+    }
+    if (countWinner > countLoser) {
+        alert("You won!")
+    }
+    else if (countWinner === countLoser) {
+        alert("It's a draw!")
+    }
+    else {
+        alert("You lost!")
+    }
+}
+
+game()
