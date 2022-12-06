0c41f5d3bbf0e554b41a4bae5c8e469d175793f3
DouglasCBS <douglascesar12@gmail.com>
2022-12-06 14:13:08 -0300

    Add answers to computer ROCK choice
    Add a function that plays the game once and answers if the computer
    plays ROCK

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3)
     if (choice === 0) {
-        return "rock"
+        return "ROCK"
     }
 
     else if (choice === 1) {
-        return "paper"
+        return "PAPER"
     }
 
     else if (choice === 2) {
-        return "scissors"
+        return "SCISSORS"
     }
 }
 
-console.log (getComputerChoice())
+let playerChoice = "ROCK"
+let computerChoice = getComputerChoice()
+
+console.log(computerChoice)
+
+function playRound(playerChoice, computerChoice) {
+    if (computerChoice === "ROCK") {
+        switch (playerChoice) {
+            case "ROCK":
+                return "It's a draw! Try again."
+                break;
+            case "PAPER":
+                return "You win! Paper beats Rock"
+                break;
+            case "SCISSORS":
+                return "You lose! Rock beats Scissors"
+        }
+    }
+}
+
+console.log(playRound(playerChoice, computerChoice))
