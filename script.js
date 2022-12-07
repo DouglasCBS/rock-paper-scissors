d50e55eeb17284801f3ab89271590e2e85098f16
DouglasCBS <douglascesar12@gmail.com>
2022-12-07 14:57:29 -0300

    Add semicolon after each statement

--- script.js
+++ script.js
@@ -1,76 +1,76 @@
 function getComputerChoice() {
-    let choice = Math.floor(Math.random() * 3)
+    let choice = Math.floor(Math.random() * 3);
     if (choice === 0) {
-        return "ROCK"
+        return "ROCK";
     }
 
     else if (choice === 1) {
-        return "PAPER"
+        return "PAPER";
     }
 
     else if (choice === 2) {
-        return "SCISSORS"
+        return "SCISSORS";
     }
-}
-let countWinner = 0
-let countLoser = 0
+};
+let countWinner = 0;
+let countLoser = 0;
 function playRound(playerChoice, computerChoice) {
     if (computerChoice === "ROCK") {
         switch (playerChoice) {
             case "ROCK":
-                return "Tie!"
+                return "Tie!";
             case "PAPER":
                 ++countWinner
-                return "You win! Paper beats Rock."
+                return "You win! Paper beats Rock.";
             case "SCISSORS":
                 ++countLoser
-                return "You lose! Rock beats Scissors."
+                return "You lose! Rock beats Scissors.";
         }
     }
     else if (computerChoice === "PAPER") {
         switch (playerChoice) {
             case "ROCK":
                 ++countLoser
-                return "You lose! Paper beats Rock."
+                return "You lose! Paper beats Rock.";
             case "PAPER":
-                return "Tie!"
+                return "Tie!";
             case "SCISSORS":
                 ++countWinner
-                return "You win! Scissors beats Paper."
+                return "You win! Scissors beats Paper.";
         }
     }
     else if (computerChoice === "SCISSORS") {
         switch (playerChoice) {
             case "ROCK":
                 ++countWinner
-                return "You win! Rock beats Scissors"
+                return "You win! Rock beats Scissors";
             case "PAPER":
                 ++countLoser
-                return "You lose! Scissors beats Paper."
+                return "You lose! Scissors beats Paper.";
             case "SCISSORS":
-                return "Tie!"
+                return "Tie!";
 
         }
     }
-}
+};
 
 function game() {
     for (let i = 0; i < 5; i++) {
-        let computerChoice = getComputerChoice()
-        let playerChoice = prompt("Rock, paper or scissors?").toUpperCase()
-        alert(playRound(playerChoice, computerChoice))
+        let computerChoice = getComputerChoice();
+        let playerChoice = prompt("Rock, paper or scissors?").toUpperCase();
+        alert(playRound(playerChoice, computerChoice));
         
         
     }
     if (countWinner > countLoser) {
-        alert("You won!")
+        alert("You won!");
     }
     else if (countWinner === countLoser) {
-        alert("It's a draw!")
+        alert("It's a draw!");
     }
     else {
-        alert("You lost!")
+        alert("You lost!");
     }
-}
+};
 
-game()
+game();
